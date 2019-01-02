14f47bc19034f3a2341451d7fc845dc8a834514b
Irfansjah Antemas <irfansjah@gmail.com>
2019-01-02 14:10:27 +0700

    baru nih

--- smartcoins/routes/web.php
+++ smartcoins/routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get("/test", function(){ return view("test");});
 /* Landing Page */
 Route::get('/', function () {
     return view('landingpage.index');
